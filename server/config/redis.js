5aeb4487aba3b0a60218494adba9191abaa5a50d
John Ortega <j.tega149@gmail.com>
2026-08-10 19:19:50 -0400

    Added cloud option for redis

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,6 +1,12 @@
 import { createClient } from 'redis';
+import { Redis } from '@upstash/redis'
 
 const redisClient = createClient({ url: process.env.REDIS_URL});
+
+const redisCloudClient = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+})
 
 redisClient.on('error', (err) => {
     console.error('Redis error:', err);
@@ -8,4 +14,6 @@ redisClient.on('error', (err) => {
 
 await redisClient.connect();
 
-export default redisClient;
+const actualRedisClient = process.env.NODE_ENV === 'production' ? redisCloudClient : redisClient;
+
+export default actualRedisClient;
